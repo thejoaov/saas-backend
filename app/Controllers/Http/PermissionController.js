8e8ea123a3e5c4a5f94e5991be74a549d44846c5
João Victor <jvictorsantos852@gmail.com>
2020-02-26 00:16:35 -0300

    feat: permissions by user

---
+++ app/Controllers/Http/PermissionController.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const UserTeam = use("App/Models/UserTeam");
+/**
+ * Resourceful controller for interacting with permissions
+ */
+class PermissionController {
+	/**
+	 * Display a single permission.
+	 * GET permissions/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async show({ request, auth }) {
+		const teamJoin = await UserTeam.query()
+			.where("team_id", request.team.id)
+			.where("user_id", auth.user.id)
+			.first();
+
+		return {
+			roles: await teamJoin.getRoles(),
+			permissions: await teamJoin.getPermissions(),
+		};
+	}
+}
+
+module.exports = PermissionController;

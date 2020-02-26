686cd025945faabfd74988e929c5ea8f96534a90
João Victor <jvictorsantos852@gmail.com>
2020-02-25 22:47:23 -0300

    feat: crud of projects

--- start/routes.js
+++ start/routes.js
@@ -24,4 +24,5 @@ Route.group(() => {
 
 Route.group(() => {
 	Route.post("invites", "InviteController.store");
+	Route.resource("projects", "ProjectController").apiOnly();
 }).middleware(["auth", "team"]);
